1d3df837e88ad8870a5b3bfba519eef0be536eb8
osboxes.org <wlin40@illinois.edu>
2017-09-15 16:08:16 -0400

    shortened names

--- graphtool/graphtool.py
+++ graphtool/graphtool.py
@@ -1,27 +1,39 @@
 import json
 import subprocess
 import sys
-
+def cut(s):
+	if len(s)>45:
+		return s[:20]+"+++++"+s[-20:]
+	else:
+		return s
 def json_parse(node):
   if len(node)>3:
-	data=node["data"]
+	data=cut(node["data"])
+	output='"'+data+'"'+";"
+	writer.write("{}\n".format(output))
 	networkData=node["networkData"]
 	request=networkData["request"]
 	initiator=request["initiator"]
+	documentURL=cut(request["documentURL"])
+	
 	if initiator["type"] == "parser":
-		url=initiator["url"]
+		url=cut(initiator["url"])
 		lineNumber=str(initiator["lineNumber"])
 		output='"'+url+'"'+" -> "+'"'+data+'"'+" [label="+'"'+lineNumber+'"'+"];"	
 		writer.write("{}\n".format(output))
 	if initiator["type"] == "script":
 		stack=initiator["stack"]
 		callFrames=stack["callFrames"]
+		count=0
 		for callFrame in callFrames:
-			url=callFrame["url"]
+			url=cut(callFrame["url"])
 #			print callFrame
 			label=str(callFrame["lineNumber"])+":"+str(callFrame["columnNumber"])
 			output='"'+url+'"'+" -> "+'"'+data+'"'+" [label="+'"'+label+'"'+"];"	
 			writer.write("{}\n".format(output))
+			count+=1
+		if (count==0):
+			output='"'+documentURL+'"'+" -> "+'"'+data+'"'+";"
 	children=node["children"]
 	for subnode in children:
 		json_parse(subnode)
